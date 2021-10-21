ef0037ea5b458c05eaf476930d2617ded00209f5
dinhhungGM <dinhhungGM@gmail.com>
2021-10-21 19:31:49 +0700

    first commit

--- app.js
+++ app.js
@@ -2,22 +2,15 @@ const express = require('express')
 const http = require('http')
 var cors = require('cors')
 const app = express()
-const bodyParser = require('body-parser')
-const path = require("path")
 var xss = require("xss")
 
 var server = http.createServer(app)
 var io = require('socket.io')(server)
 
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 
-if(process.env.NODE_ENV==='production'){
-	app.use(express.static(__dirname+"/build"))
-	app.get("*", (req, res) => {
-		res.sendFile(path.join(__dirname+"/build/index.html"))
-	})
-}
+
 app.set('port', (process.env.PORT || 4001))
 
 sanitizeString = (str) => {
